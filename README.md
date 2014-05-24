225627603a431b5ac6540ecf3367ddda30dbe93a
icchw <kh.wada@gmail.com>
2014-05-24 16:42:35 +0900

    change input parameters

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 each_line_groovy
 ================
-groovy -c UTF-8 each_line.groovy template.sql
+groovy -c UTF-8 each_line.groovy input.txt template.sql UTF-8
--- each_line.groovy
+++ each_line.groovy
@@ -1,18 +1,21 @@
-def inputFile = new File('input.txt')
+// args[0]:input filename
+// args[1]:template filename
+// args[2]:character code
+
+def inputFile = new File(args[0])
 def inputs = []
-inputFile.readLines("UTF-8").each {
+inputFile.readLines(args[2]).each {
   if (!it.empty) {
     inputs << it.split('\t')
   }
 }
 
-def templateFileName = args[0]
+def templateFileName = args[1]
 def f = new File(templateFileName)
 def engine = new groovy.text.SimpleTemplateEngine()
-def binding = ['values' :  inputs]
+def binding = ['values' :  inputs, 'inputFileName' : args[0], 'templateFileName' : templateFileName ]
 def template = engine.createTemplate(f).make(binding)
 
 def date = new Date().format('yyyyMMdd_HHmmss')
-def template_extension = templateFileName.replaceAll(/.*\.(.*)/){it[1]}
-def result = new File(date + "." + template_extension)
-result.write(template.toString(), "UTF-8")
+def result = new File(date + '_'  + templateFileName)
+result.write(template.toString(), args[2])
